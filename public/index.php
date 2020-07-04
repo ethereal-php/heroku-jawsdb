744ef6efee0394008e3a11d2cf3542509ee8fd13
Shin Maung Maung <shinmaungmaung@gmail.com>
2020-07-04 12:01:14 +0630

    First Commit

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new Ethereal\Foundation\Application(
+    dirname(__DIR__)
+);
---
+++ src/Ethereal/Support/helper.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists('env')) {
+    function env(string $name, $default = null)
+    {
+        return $_ENV[$name] ?? getenv($name) ?: $default;
+    }
+}
